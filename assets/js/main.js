9d97bf408facd4efd992388355f27ca2703fe63d
sachariase99 <126781566+sachariase99@users.noreply.github.com>
2023-08-28 11:29:16 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,20 @@
 // 1.
-var navn = "Jannick";
+const navn = "Jannick";
 console.log(navn);
 
 // 2.
-var x = 2;
-var y = 5;
-var sum = x + y;
+const x = 2;
+const y = 5;
+const sum = x + y;
 console.log(sum);
 
 // 3.
-var alder = 24;
+let alder = 24;
 alder += 5;
 console.log(alder);
 
 // 4.
-var myNumber = 1;
+let myNumber = 1;
 
 myNumber++; //Går til 2
 console.log(myNumber);
