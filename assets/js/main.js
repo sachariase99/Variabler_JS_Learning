ad1612418fc716167112fb24e9c47a0bbdaf6f4f
sachariase99 <126781566+sachariase99@users.noreply.github.com>
2023-08-28 11:51:17 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,36 @@
 // 1.
+console.group('Opgave 1')
+
 const navn = "Jannick";
+
 console.log(navn);
 
+console.groupEnd('Opgave 1')
+
 // 2.
+console.group('Opgave 2')
+
 const x = 2;
 const y = 5;
 const sum = x + y;
+
 console.log(sum);
 
+console.groupEnd('Opgave 2')
+
 // 3.
+console.group('Opgave 3')
+
 let alder = 24;
+
 alder += 5;
 console.log(alder);
 
+console.groupEnd('Opgave 3')
+
 // 4.
+console.group('Opgave 4')
+
 let myNumber = 1;
 
 myNumber++; //Går til 2
@@ -21,3 +38,5 @@ console.log(myNumber);
 
 myNumber--; //Går tilbage til 1
 console.log(myNumber);
+
+console.groupEnd('Opgave 4')
