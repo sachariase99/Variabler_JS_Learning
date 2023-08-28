021411aab89c1e4fc0238c47b74275cd4a3dc2ce
sachariase99 <126781566+sachariase99@users.noreply.github.com>
2023-08-28 11:09:37 +0200

    Javascript

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+// 1.
+var navn = "Jannick";
+console.log(navn);
+
+// 2.
+var x = 2;
+var y = 5;
+var sum = x + y;
+console.log(sum);
+
+// 3.
+var alder = 24;
+alder += 5;
+console.log(alder);
+
+// 4.
+var myNumber = 1;
+
+myNumber++; //Går til 2
+console.log(myNumber);
+
+myNumber--; //Går tilbage til 1
+console.log(myNumber);
